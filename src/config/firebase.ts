7453e2984e605a776f6e9468896df19c6dc0f171
upeng5 <mism090@outlook.com>
2021-10-06 18:38:55 +0300

    Modify .env file

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 // import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const options = {
-  apiKey: "AIzaSyDCNKIyHED_r2Fq83IzXtLNyIbkstPXsMs",
-  authDomain: "ts-ecom-app.firebaseapp.com",
-  projectId: "ts-ecom-app",
-  storageBucket: "ts-ecom-app.appspot.com",
-  messagingSenderId: "236826803319",
-  appId: "1:236826803319:web:61f7a5e88342c3bebd5a72",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(options);
